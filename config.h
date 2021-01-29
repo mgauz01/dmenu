2a9c80cea263825d3414395e1a304d6b611e238d
Michael Gringauz <michael.gringauz@mail.utoronto.ca>
2021-01-28 21:11:40 -0500

    so far the numbers case sensetive and height patch have been implemented

--- config.h
+++ config.h
@@ -25,6 +25,10 @@ static const unsigned int alphas[SchemeLast][2] = {
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 12;
+
 
 /*
  * Characters not considered part of a word while deleting words
