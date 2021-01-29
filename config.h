75bc125e9e6b900de7ca145a8bdaad328d64087b
Michael Gringauz <michael.gringauz@mail.utoronto.ca>
2021-01-28 22:07:09 -0500

    after fuzzy match and fuzzy colouring was implemented

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static const char *fonts[] = {
 	"monospace:size=10",
 	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
@@ -12,8 +13,10 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#96b5b4", "#181818" },
+	[SchemeSel] = { "#d7d7d7", "#4d4d4d" },
+    [SchemeSelHighlight] = { "#ffc978", "#005577" },
+    [SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
@@ -26,8 +29,8 @@ static const unsigned int alphas[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
-static unsigned int min_lineheight = 12;
+static unsigned int lineheight = 30;
+static unsigned int min_lineheight = 24;
 
 
 /*
