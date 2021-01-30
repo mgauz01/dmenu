b610291ee82d35f8b07e3ae872417ab7bc79abf8
Michael Gringauz <michael.gringauz@mail.utoronto.ca>
2021-01-29 22:02:58 -0500

    added highlight patch

--- config.h
+++ config.h
@@ -3,7 +3,6 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static const char *fonts[] = {
 	"monospace:size=10",
 	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
@@ -13,11 +12,13 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#96b5b4", "#181818" },
-	[SchemeSel] = { "#d7d7d7", "#4d4d4d" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
     [SchemeSelHighlight] = { "#ffc978", "#005577" },
-    [SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+    [SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+
 };
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
@@ -29,8 +30,8 @@ static const unsigned int alphas[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 30;
-static unsigned int min_lineheight = 24;
+static unsigned int lineheight = 29;
+static unsigned int min_lineheight = 12;
 
 
 /*
